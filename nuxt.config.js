d0b3dd53392efd1f60ac580d0d1e1d91baded8d2
Igor Sushko <igor_sushko@yahoo.com>
2022-04-27 19:26:04 +0500

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,17 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
+  target: 'static',
+  router: {
+    base: '/charts/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'charts',
     htmlAttrs: {
       lang: 'en'
     },
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
